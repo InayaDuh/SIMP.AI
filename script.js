cb85ed9e74bffefe7ca27bb1aa1eb9ebbfc30ccf
InayaDuh! <surainaya3@outlook.com>
2026-09-06 09:35:59 +0000

    2ND commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("Hello");
 const startbutton = document.getElementById("startbutton");
 const message = document.getElementById("message")
-startbutton.addEventListener("click", function () {message.textContent = "Let's Find This Day of Yours";
+startbutton.addEventListener("click", function () {message.textContent = "Let's Find This Day of Yours"; questionBank.style.display = "block";
 	console.log("Yup button clicked!");
 });
+const questionBank = document.getElementById("question-bank");
+questionBank.style.display = "none";
+setTimeout(function () {window.location.href = "questions.html";}, 5000);
