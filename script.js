e1fef7b05618b443fc8fcdd560c8b3e2031164a0
InayaDuh! <surainaya3@outlook.com>
2026-09-07 08:52:13 +0000

    3rd

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("Hello");
 const startbutton = document.getElementById("startbutton");
 const message = document.getElementById("message")
-startbutton.addEventListener("click", function () {message.textContent = "Let's Find This Day of Yours"; questionBank.style.display = "block";
-	console.log("Yup button clicked!");
-});
 const questionBank = document.getElementById("question-bank");
 questionBank.style.display = "none";
+startbutton.addEventListener("click", function () {message.textContent = "Let's Find This Day of Yours"; questionBank.style.display = "block";
+	console.log("Yup button clicked!");
 setTimeout(function () {window.location.href = "questions.html";}, 5000);
+});
